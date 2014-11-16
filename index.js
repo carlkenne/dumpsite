39b5c3327983669833ee0ebefe7f8cc143e73576
carl kenne <carl.kenne@gmail.com>
2014-11-16 21:15:11 +0800

    added capability to take a screenshot of the webpage

--- index.js
+++ index.js
@@ -1,19 +1,34 @@
 var fs = require('fs');
 var rp = require('request-promise');
 var rimraf = require('rimraf');
+var webshot = require('webshot');
+
 
 var sitmapUrl = "/dumpsite/sitemap.json"; // change this url to a proper url
 var destination = "./sitedump/";
 rimraf.sync(destination);
 fs.mkdir(destination);
 
-var domain = process.argv.filter(function(arg) { return arg.indexOf("domain=") == 0; })[0];
-domain = domain.replace("domain=", "");
+function getArg(_arg, _default){
+	var foundArg = process.argv.filter(function(a) { return a.indexOf(_arg) == 0; });
+	if(foundArg.length > 0){
+		return foundArg[0].replace(_arg, "");
+	}
+	return _default;
+}
+
+var domain = getArg("domain=","missing domain");
+var screenshot = getArg("screenshot=", "false");
 
 function sitemapDownloaded(data) {
 	console.log("SiteMap:", JSON.parse(data));
 
 	JSON.parse(data).list.forEach(function(url) {
+		console.log("screenshot: " + screenshot);
+		if(screenshot == "true"){
+			webshot(domain + url, urlToFileName(url, ".png"), function(err) {});	
+		}
+		
 		rp(domain + url)
 			.then(function(html) { pageDownloaded(url,html) })
     		.catch(console.error);
@@ -21,9 +36,15 @@ function sitemapDownloaded(data) {
 }
 
 function pageDownloaded(url, html){
-	var path = destination + url.replace("/","").split("/").join("_").replace(".html","") + ".html";
+	var path = urlToFileName(url, ".html");
 	console.log("Output to: " + path);
 	fs.writeFile(path, html, {flags:"w"});
+}
+
+function urlToFileName(url, type){
+	return destination + url.replace("/","")
+		.split("/").join("_")
+		.replace(".html","") + type;
 }
 
 console.log("SiteMapUrl: " + domain + sitmapUrl);
